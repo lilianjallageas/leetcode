008fa5fc65921718bef598d19bef2bf56bd6c1f8
Lilian Jallageas <lilian.jallageas@showtime.net>
2020-02-24 17:06:55 -0500

    no message

---
+++ javascript/0001_TwoSum.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * Problem #1
+ * Problem URL: https://leetcode.com/problems/two-sum/
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let indexA = 0; indexA < nums.length; indexA++){
+        for (let indexB = indexA+1; indexB < nums.length; indexB++){
+            if (indexA != indexB){
+               if (nums[indexA]+nums[indexB] == target) { return [indexA, indexB] } 
+            }
+        }
+    }
+};
