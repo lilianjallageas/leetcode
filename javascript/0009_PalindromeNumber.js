43f59fc45e124531f503e9e95850c4ea4e2c75dd
Lilian Jallageas <lilian.jallageas@showtime.net>
2020-02-25 13:45:59 -0500

    Solved Problem #9

---
+++ javascript/0009_PalindromeNumber.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * Problem #9
+ * Problem URL: https://leetcode.com/problems/palindrome-number/
+ *
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+
+    // Variables
+    var arr = x.toString().split('');
+    var i = 0
+    var j = arr.length-1;
+
+    // Going through the array 
+    while (i<j) {
+        if (arr[i] != arr[j]) return false;
+        i++; 
+        j--;
+    }
+
+    return true;
+};
