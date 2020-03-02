87ff978b601cebbe903fa9a8b5d02699f44d50ab
Lilian Jallageas <lilianjallageas@users.noreply.github.com>
2020-03-02 16:15:48 -0500

    Create 0011_ContainerWithMostWater.js
    Solving problem #11

---
+++ javascript/0011_ContainerWithMostWater.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * Problem #11
+ * Problem URL: https://leetcode.com/problems/container-with-most-water/
+ *
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    var max_water = 0;
+    for (let i=0; i < height.length-1; i++) {
+        for (let j=i+1; j < height.length; j++) {
+            water = (j-i) * Math.min(height[i],height[j]);
+            if (water > max_water) { max_water = water; }
+        }
+    }
+    return max_water;
+};
