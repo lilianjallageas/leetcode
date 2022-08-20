67bd400a29fefe733c84551edbef941be91e9d1b
Lilian Jallageas <lilian.jallageas@free.fr>
2022-08-20 14:23:24 -0700

    Improving solution to problem #67, by using Arrays instead of strings.

--- javascript/0067_AddBinary.js
+++ javascript/0067_AddBinary.js
@@ -8,26 +8,31 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-	let output = "";
 
-	// Aligning the strings, so they're easier to parse
-	let maxLength = Math.max(a.length, b.length);
-	let difference = Math.abs(a.length-b.length);
-	(a.length < b.length) ? a = "0".repeat(difference).concat(a) : b = "0".repeat(difference).concat(b);
+	// Using Arrays (instead of strings), for performance purpose
+	let A = a.split('');
+	let B = b.split('');
 
-	// Parsing both strings (from the end) to do the binary addition
+	// Aligning the arrays (with zeros), so they're easier to parse
+	let maxLength = Math.max(A.length, B.length);
+	let difference = Math.abs(A.length - B.length);
+	let arrayOfZeros = new Array(difference).fill(0);
+	(A.length < B.length) ? A = [...arrayOfZeros, ...A] : B = [...arrayOfZeros, ...B];
+	let output = new Array(maxLength).fill("0");
+
+	// Parsing both arrays to do the binary addition
 	let carry = 0;
-	for (let index = maxLength-1; index >= 0; index--) {
-		let aValue = parseInt(a.charAt(index));
-		let bValue = parseInt(b.charAt(index));
+	for (let index = maxLength-1; index >= 0 ; index--) {
+		let aValue = parseInt(A[index]);
+		let bValue = parseInt(B[index]);
 		let localAddition = carry + aValue + bValue;
-		if (localAddition === 0) { output = "0".concat(output); carry = 0; }
-		if (localAddition === 1) { output = "1".concat(output); carry = 0; }
-		if (localAddition === 2) { output = "0".concat(output); carry = 1; }
-		if (localAddition === 3) { output = "1".concat(output); carry = 1; }
+		if (localAddition === 0) { output[index] = "0"; carry = 0; }
+		if (localAddition === 1) { output[index] = "1"; carry = 0; }
+		if (localAddition === 2) { output[index] = "0"; carry = 1; }
+		if (localAddition === 3) { output[index] = "1"; carry = 1; }
 	}
 	// Adding the last carried value
-	if (carry === 1) { output = "1".concat(output); }
+	if (carry === 1) { output.unshift("1"); }
 
-	return output;
+	return output.join('');
 };
